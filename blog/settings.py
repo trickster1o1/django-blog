c113f8885fbd8ff998687bdee3443f5123625d12
trickster1o1 <nischaltuladhar15@gmail.com>
2023-04-02 18:55:23 +0545

    migrations and user app created for registration

--- blog/settings.py
+++ blog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'mainBlog.apps.MainblogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mainBlog/views.py
+++ mainBlog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-
-posts = [
-    {
-        'author': 'Nischal Tuladhar',
-        'title': 'The Fox',
-        'content': 'A quick brown fox jumps over the lazy dog',
-        'date_posted': '2022-12-12'
-    },
-    {
-        'author': 'Sumnima Sakya',
-        'title': 'Fire Fox',
-        'content': 'A quick fire fox jumps over the lazy dog',
-        'date_posted': '2023-2-12'
-    },
-]
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'mainBlog/home.html',context)
 
