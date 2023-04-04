4f29c320fecd8eb893128b60704cc5c10f1a9a26
trickster1o1 <nischaltuladhar15@gmail.com>
2023-04-04 19:12:33 +0545

    auto profile creation and image dir setup

--- blog/urls.py
+++ blog/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as users_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,6 @@ urlpatterns = [
     path('profile/',users_views.profile, name='profile'),
     path('', include('mainBlog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
